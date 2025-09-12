7bd6d4a3d1c5031e8876b0a56a98ff0a250554b5
Kerim Allaberenov <allaberenov@172.31.132.153-vpn.dhcp.yndx.net>
2025-09-12 22:45:54 +0300

    version 1.0

---
+++ sem01-intro/aplusb_preprocessed.c
@@ -0,0 +1,18 @@
+# 1 "aplusb.c"
+# 1 "<built-in>" 1
+# 1 "<built-in>" 3
+# 418 "<built-in>" 3
+# 1 "<command line>" 1
+# 1 "<built-in>" 2
+# 1 "aplusb.c" 2
+
+
+
+
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    return 0;
+}
